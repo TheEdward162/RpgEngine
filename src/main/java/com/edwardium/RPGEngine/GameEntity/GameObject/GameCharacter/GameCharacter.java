016a37057c11b0c273322c0df21953712e890d95
Eduard Lavuš <lavusedu@fel.cvut.cz>
2018-03-09 22:01:03 +0100

    Added calls to super. Moved name to GameObject class.

--- src/main/java/com/edwardium/RPGEngine/GameEntity/GameObject/GameCharacter/GameCharacter.java
+++ src/main/java/com/edwardium/RPGEngine/GameEntity/GameObject/GameCharacter/GameCharacter.java
@@ -1,16 +1,15 @@
-package com.edwardium.RPGEngine.GameObject.GameCharacter;
+package com.edwardium.RPGEngine.GameEntity.GameObject.GameCharacter;
 
-import com.edwardium.RPGEngine.GameObject.GameInventory;
-import com.edwardium.RPGEngine.GameObject.GameObject;
+import com.edwardium.RPGEngine.GameEntity.GameInventory;
+import com.edwardium.RPGEngine.GameEntity.GameObject.GameObject;
 import com.edwardium.RPGEngine.Renderer.Renderer;
 import com.edwardium.RPGEngine.Renderer.TextureInfo;
 import com.edwardium.RPGEngine.Vector2D;
 
 public class GameCharacter extends GameObject {
-	protected float maxWalkSpeed = 350f;
+	public float maxWalkSpeed = 350f;
 	private Vector2D walkVector = new Vector2D(0, 0);
 
-	public String name = "";
 	public GameInventory inventory;
 
 	public GameCharacter() {
@@ -44,21 +43,24 @@ public class GameCharacter extends GameObject {
 		this.velocity.add(this.walkVector);
 		this.walkVector.set(0, 0);
 
-		this.position.add(Vector2D.multiply(this.velocity, elapsedTime));
-		this.velocity.multiply(velocityDiminishFactor * elapsedTime);
+		super.update(elapsedTime, velocityDiminishFactor);
 	}
 
 	@Override
 	public void render(Renderer gameRenderer) {
-		// shadow
-		gameRenderer.drawCircle(50f, this.position, new float[] {0f, 0f, 0f, 0.3f}, null);
+		if (true || isDrawn) {
+			// shadow
+			gameRenderer.drawCircle(50f, this.position, new float[]{0f, 0f, 0f, 0.3f}, new TextureInfo("default"));
 
-		// body
-		gameRenderer.drawRectangle(this.position, new Vector2D(15, 25), this.getFacingDirection().getAngle(), new float[] {1f, 1f, 0f, 1f}, null);
+			// body
+			gameRenderer.drawRectangle(this.position, new Vector2D(15, 25), this.getFacingDirection().getAngle(), new float[]{1f, 1f, 0f, 1f}, new TextureInfo("default"));
 
-		// facing direction
-		gameRenderer.drawLine(this.position, Vector2D.add(this.position, this.getFacingDirection().setMagnitude(30)), 2f, new float[] { 1f, 0f, 0f, 1f });
+			// facing direction
+			gameRenderer.drawLine(this.position, Vector2D.add(this.position, this.getFacingDirection().setMagnitude(30)), 2f, new float[]{1f, 0f, 0f, 1f});
 
-		gameRenderer.drawString(gameRenderer.basicFont, "Hello, world!", new Vector2D(0, 100), new Vector2D(1, 1), new float[] { 0f, 1f, 0f, 1f });
+			gameRenderer.drawString(gameRenderer.basicFont, this.name, new Vector2D(30, -30).add(this.position), new Vector2D(1, 1), new float[]{0f, 1f, 0f, 1f});
+		}
+
+		super.render(gameRenderer);
 	}
 }
