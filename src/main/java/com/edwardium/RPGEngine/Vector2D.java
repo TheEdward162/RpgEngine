9b1ccecac8a5b80a22c7d14dbdb0138cab6c42a7
Eduard Lavuš <lavusedu@fel.cvut.cz>
2018-03-09 20:34:57 +0100

    Added absolutize function.

--- src/main/java/com/edwardium/RPGEngine/Vector2D.java
+++ src/main/java/com/edwardium/RPGEngine/Vector2D.java
@@ -18,13 +18,8 @@ public class Vector2D {
 	public static Vector2D inverse(Vector2D a) {
 		return new Vector2D(a).inverse();
 	}
-
-	public static Vector2D normalize(Vector2D a) {
-		return new Vector2D(a).normalize();
-	}
-
-	public static Vector2D limit(Vector2D a, float limit) {
-		return new Vector2D(a).limit(limit);
+	public static Vector2D absolutize(Vector2D a) {
+		return new Vector2D(a).absolutize();
 	}
 
 	public static Vector2D add(Vector2D a, Vector2D b) {
@@ -38,19 +33,22 @@ public class Vector2D {
 
 		return newArray;
 	}
-
 	public static Vector2D subtract(Vector2D a, Vector2D b) {
 		return new Vector2D(a).subtract(b);
 	}
-
 	public static Vector2D multiply(Vector2D a, float m) {
 		return new Vector2D(a).multiply(m);
 	}
-
 	public static Vector2D divide(Vector2D a, float d) {
 		return new Vector2D(a).divide(d);
 	}
 
+	public static Vector2D limit(Vector2D a, float limit) {
+		return new Vector2D(a).limit(limit);
+	}
+	public static Vector2D normalize(Vector2D a) {
+		return new Vector2D(a).normalize();
+	}
 	public static Vector2D scale(Vector2D a, float x, float y) { return new Vector2D(a).scale(x, y); }
 	public static Vector2D scale(Vector2D a, Vector2D scaleVector) { return new Vector2D(a).scale(scaleVector); }
 
@@ -60,7 +58,6 @@ public class Vector2D {
 	public static float dot(Vector2D a, Vector2D b) {
 		return a.dot(b);
 	}
-
 	public static float angleBetween(Vector2D a, Vector2D b) { return a.angleBetween(b); }
 
 	// Constructors
@@ -95,23 +92,19 @@ public class Vector2D {
 	public float getX() {
 		return posX;
 	}
-
 	public Vector2D setX(float x) {
 		this.posX = x;
 
 		return this;
 	}
-
 	public float getY() {
 		return posY;
 	}
-
 	public Vector2D setY(float y) {
 		this.posY = y;
 
 		return this;
 	}
-
 	public Vector2D set(float x, float y) {
 		this.setX(x);
 		this.setY(y);
@@ -122,7 +115,6 @@ public class Vector2D {
 	public float getAngle() {
 		return (float)Math.atan2(posY, posX);
 	}
-
 	public Vector2D setAngle(float angle) {
 		float newX = (float)(Math.cos(angle) * getMagnitude());
 		float newY = (float)(Math.sin(angle) * getMagnitude());
@@ -132,7 +124,6 @@ public class Vector2D {
 
 		return this;
 	}
-
 	public Vector2D rotateBy(float angle) {
 		return this.setAngle(this.getAngle() + angle);
 	}
@@ -140,7 +131,6 @@ public class Vector2D {
 	public float getMagnitude() {
 		return (float)Math.sqrt(posX * posX + posY * posY);
 	}
-
 	public Vector2D setMagnitude(float magnitude) {
 		float newX = (float)(Math.cos(getAngle()) * magnitude);
 		float newY = (float)(Math.sin(getAngle()) * magnitude);
@@ -159,15 +149,9 @@ public class Vector2D {
 	public Vector2D inverse() {
 		return this.multiply(-1);
 	}
-
-	public Vector2D normalize() {
-		return this.setMagnitude(1);
-	}
-
-	public Vector2D limit(float limit) {
-		if (this.getMagnitude() > limit) {
-			this.setMagnitude(limit);
-		}
+	public Vector2D absolutize() {
+		this.posX = Math.abs(this.posX);
+		this.posY = Math.abs(this.posY);
 
 		return this;
 	}
@@ -184,26 +168,32 @@ public class Vector2D {
 			v.add(this);
 		}
 	}
-
 	public Vector2D subtract(Vector2D other) {
 		this.posX -= other.posX;
 		this.posY -= other.posY;
 
 		return this;
 	}
-
-	// Scalar
 	public Vector2D multiply(float m) {
 		this.posX *= m;
 		this.posY *= m;
 
 		return this;
 	}
-
 	public Vector2D divide(float d) {
 		return this.multiply(1 / d);
 	}
 
+	public Vector2D limit(float limit) {
+		if (this.getMagnitude() > limit) {
+			this.setMagnitude(limit);
+		}
+
+		return this;
+	}
+	public Vector2D normalize() {
+		return this.setMagnitude(1);
+	}
 	public Vector2D scale(float x, float y) {
 		this.posX *= x;
 		this.posY *= y;
@@ -224,7 +214,6 @@ public class Vector2D {
 	public float dot(Vector2D other) {
 		return this.posX * other.posX + this.posY * other.posY;
 	}
-
 	public float angleBetween(Vector2D other) {
 		// How this works is
 		// Dot product can be written as a . b = |a| * |b| * cos t, where t is the angle between a and b
@@ -239,7 +228,6 @@ public class Vector2D {
 
 		return (float)Math.atan2(-cross, dot);
 	}
-
 
 	@Override
 	public String toString() {
